91fbdf9e317554ed166c4bde01711c8a4f9b730c
hang songaph <cygames.co.jp>
2018-05-02 18:34:49 +0900

    PerformanceCheck追加

---
+++ Unity/PerformanceCheck/Assets/Script/EnumCheck.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnumCheck : MonoBehaviour {
+
+	enum State {
+		XXX,
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		State x = State.XXX;
+
+		PerformanceChecker perfChecker = new PerformanceChecker("== vs Equals in Enum", 10000);
+		perfChecker.CheckAction ( "==", (i) => {
+			if (x == State.XXX ) {
+			}
+		});
+
+		perfChecker.CheckAction ( "Equals", (i) => {
+			if ( x.Equals(State.XXX) ){
+			}
+		});
+
+		perfChecker.ShowResult();
+	}
+}
