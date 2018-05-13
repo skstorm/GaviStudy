f9a2d3d2e441ddb51be200b59b4fb1b9a44c03da
Han_Sungkap <realskstorm@gmail.com>
2018-05-13 19:12:57 +0900

    Util관련 클래스 추가

---
+++ Unity/Framework/Assets/Scripts/FrameWork/Ark/Util/SystemText.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ark.Util
+{
+	public enum SystemText
+	{
+		EMPTY,
+		SLASH,
+		HYPHEN,
+		CR,
+		LF,
+		CRLF,
+		DOUBLE_SLASH,
+		PLUS,
+		COMMA,
+		TAB,
+		SPACE,
+		PIPE,
+	}
+
+	public static partial class SystemTextUtil
+	{
+		static string[] StrSystemText =
+		{
+			"",
+			"/",
+			"-",
+			"\r",
+			"\n",
+			"\r\n",
+			"//",
+			"+",
+			",",
+			"\t",
+			" ",
+			"|",
+		};
+
+		public static string Text(this SystemText value, params string[] args)
+		{
+			if (args.Length > 0)
+			{
+				return string.Format(StrSystemText[(int)value], args);
+			}
+			else
+			{
+				return StrSystemText[(int)value];
+			}
+		}
+	}
+}
