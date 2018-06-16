49ccc3f64255a2dffafd375fd802e0730c848c16
Han_Sungkap <realskstorm@gmail.com>
2018-06-17 00:13:44 +0900

    베이스 큐, 뷰풀 작성

---
+++ Unity/Framework/Assets/Scripts/FrameWork/Ark/LifeCycle/BaseQueue.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace Ark.LifeCycle
+{
+	public class BaseQueue<TData>
+	{
+		protected List<TData> _bookList = null;
+
+		protected BaseQueue()
+		{
+		}
+
+		public void ListInit()
+		{
+			_bookList = new List<TData>();
+		}
+
+		public void ListClear()
+		{
+			if (_bookList.Count > 0)
+			{
+				_bookList.Clear();
+			}
+		}
+
+		public void Book(TData obj)
+		{
+			_bookList.Add(obj);
+		}
+
+		public TData Get(int i)
+		{
+			return _bookList[i];
+		}
+
+		public int GetBookLength()
+		{
+			return _bookList.Count;
+		}
+
+		public void ListRelease()
+		{
+			_bookList.Clear();
+			_bookList = null;
+		}
+	}
+}
---
+++ Unity/Framework/Assets/Scripts/FrameWork/Ark/LifeCycle/BaseViewPool.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ark.LifeCycle
+{
+	public class BaseViewPool : MonoBehaviour
+	{
+		// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- //
+		//! View追加（複数）
+		protected void AddView<TView>(TView viewPrefab, List<TView> list, int addNum) where TView : EntityView
+		{
+			for (int i = 0; i < addNum; ++i)
+			{
+				AddView(viewPrefab, list);
+			}
+		}
+
+		// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- //
+		//! View追加（単体）
+		protected TView AddView<TView, TViewPrefab>(TViewPrefab viewPrefab, List<TView> list) where TView : class, IEntityView where TViewPrefab : EntityView
+		{
+			TView view = Instantiate(viewPrefab, Vector3.zero, Quaternion.identity) as TView;
+			view.InitView(transform);
+			view.DeactiveView();
+			list.Add(view);
+			return view;
+		}
+
+		// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- //
+		//! ユニットを取得する
+		protected TView GetView<TView, TViewPrefab>(List<TView> list, TViewPrefab viewPrefab) where TView : class, IEntityView where TViewPrefab : EntityView
+		{
+			TView view = null;
+			// リストからViewを取得する
+			for (int i = 0; i < list.Count; ++i)
+			{
+				if (!list[i].IsViewActive)
+				{
+					view = list[i];
+					break;
+				}
+			}
+			// リストになかったらViewを作る
+			if (view == null)
+			{
+				view = AddView(viewPrefab, list);
+			}
+
+			return view;
+		}
+
+		// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- //
+		//! Viewを活性化する
+		protected void ActiveView<TView>(TView view) where TView : class, IEntityViewOrder
+		{
+			view.ActiveView();
+		}
+	}
+}
