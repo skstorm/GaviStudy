1b6dbd9bdb64e7b8914655f16535a3658e63c257
hang_songaph <hang_songaph>
2021-08-17 14:25:48 +0900

    Etc例題

---
+++ Unity/Practice/Assets/Script/EtcMain/Covariance.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace HanPractice
+{
+    public class Covariance : BaseExampleClass
+    {
+        public override void Run()
+        {
+            Debug.Log("Convariance Run");
+
+            ICreate<Dog> dogCreator = new DogFactory();
+            ICreate<Animal> creator = dogCreator;
+            Animal ani = creator.Create();
+            ani.Move();
+
+            IEnumerable<string> strings = new List<string>();
+            IEnumerable<object> objects = strings;
+            objects = new List<string>();
+
+            Func<Animal, Animal> testFunc = delegate (Animal a)
+            {
+                return a;
+            };
+
+            testFunc(new Dog());
+
+            List<ICreate<Animal>> animalList = null;
+            List<ICreate<Dog>> doglList = null;
+            // Error
+//            animalList = doglList;
+        }
+
+    }
+
+    class Animal
+    {
+        public int Age { get; set; }
+        public virtual void Move() { Debug.Log("Animal Move"); }
+    }
+    class Dog : Animal
+    {
+        public void Bark() { }
+        public override void Move() { Debug.Log("Dog Move"); }
+    }
+    class K9 : Dog
+    {
+        public void FindDrug() { }
+        public override void Move() { Debug.Log("K9 Move"); }
+    }
+
+    // 제네릭 인터페이스: Covariance 
+    interface ICreate<out T>
+    {
+        T Create();
+    }
+
+    class DogFactory : ICreate<Dog>
+    {
+        public Dog Create()
+        {
+            return new Dog();
+        }
+    }
+}
---
+++ Unity/Practice/Assets/Script/EtcMain/EtcMain.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System;
+
+namespace HanPractice
+{
+    public class EtcMain : MonoBehaviour
+    {
+        [SerializeField]
+        private ExampleInfo[] exampleInfos = null;
+
+        // Start is called before the first frame update
+        void OnEnable()
+        {
+            for(int i=0; i< exampleInfos.Length; ++i)
+            {
+                var info = exampleInfos[i];
+                if(info.IsRun)
+                {
+                    info.Example.Run();
+                }
+            }
+        }
+    }
+
+    [Serializable]
+    public class ExampleInfo
+    {
+        [SerializeField]
+        private string name = string.Empty;
+
+        [SerializeField]
+        private bool isRun = true;
+        public bool IsRun { get { return isRun; } }
+
+        [SerializeField]
+        private BaseExampleClass example = null;
+        public BaseExampleClass Example { get { return example; } }
+    }
+
+    [Serializable]
+    public class BaseExampleClass: MonoBehaviour
+    {
+        public virtual void Run() { }
+    }
+}
