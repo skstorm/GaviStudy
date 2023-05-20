e210cfe76eecbea245fa4599c7acc08eb0bb02a8
hang-songaph <hang_songaph@cygames.co.jp>
2023-05-20 18:51:55 +0900

    기존 싱글톤 매니저 클래스 이름 변경

---
+++ Unity/Practice/Assets/Script/TestSingletonManager/TestSingletonManager.cs
@@ -0,0 +1,46 @@
+﻿using HanPractice;
+using UnityEngine;
+
+namespace TestSingleton
+{
+    public class TestSingletonManager : BaseExampleClass
+    {
+        public override void Run()
+        {
+            {
+                TestSingletonTree<TestTreeA> tree = new TestSingletonTree<TestTreeA>();
+
+                {
+                    TempDataA tempDataA = tree.Get<TempDataA>();
+                    Debug.Log(tempDataA.A);
+                }
+
+                {
+                    TempDataA1 tempDataA1 = tree.Get<TempDataA1>();
+                    Debug.Log(tempDataA1.A1);
+                }
+            }
+
+            {
+                TestSingletonTree<TestTreeAA> tree = new TestSingletonTree<TestTreeAA>();
+                {
+                    TempDataA tempDataA = tree.Get<TempDataA>();
+                    Debug.Log(tempDataA.A);
+                }
+
+                {
+                    TempDataA1 tempDataA1 = tree.Get<TempDataA1>();
+                    Debug.Log(tempDataA1.A1);
+                }
+            }
+
+            {
+                TestSingletonTree<TestTreeB> tree = new TestSingletonTree<TestTreeB>();
+                {
+                    var tempDataA = tree.Get<TempDataB>();
+                    Debug.Log(tempDataA.B);
+                }
+            }
+        }
+    }
+}
