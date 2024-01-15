c653b462d0f6018fc8c14cc61b843c23affc994c
han <realskstorm@gmail.com>
2024-01-15 22:48:10 +0900

    Fps측정 클래스 작성

--- Unity/GameJam/Assets/Scripts/GameFsm.cs
+++ Unity/GameJam/Assets/Scripts/GameFsm.cs
@@ -1,11 +1,16 @@
+using UnityEngine;
+
 namespace GameJam
 {
     public class GameFsm : BaseFsm
     {
         private async void Awake()
         {
-            DontDestroyOnLoad(this);
             Localize.LanguageKind = ELanguageKind.En;
+            Application.targetFrameRate = 60;
+
+            DontDestroyOnLoad(this);
+
             var startState = Util.LoadScenePrefab<TitleSceneState>(Const.PathTitleScenePrefab, this);
             await fsm(startState);
         }
